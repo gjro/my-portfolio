c573312d96587c1378990804dec7c4ac559f7b81
gjro <gilbertojr.ro@gmail.com>
2021-09-19 22:25:26 -0300

    Toggle button semi-done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,10 @@ import styles from "./style.module.scss";
 import Logo from "../../iconComponents/LogoIcon";
 
 function Header() {
+	function handleToggle() {
+		console.log("toggle");
+	}
+
 	return (
 		<header className={styles.headerContainer}>
 			<div>
@@ -9,30 +13,31 @@ function Header() {
 					<Logo className={styles.logo} />
 				</a>
 			</div>
-			<div className={`${styles.menuContaier} ${styles.on}`}></div>
-			<div className={styles.menuToggle}>
-				<div className={styles.row1}></div>
-				<div className={styles.row2}></div>
-				<div className={styles.row3}></div>
+			<div className={`${styles.menuContainer} ${styles.on}`}>
+				<div onClick={handleToggle} className={styles.menuToggle}>
+					<div className={styles.row1}></div>
+					<div className={styles.row2}></div>
+					<div className={styles.row3}></div>
+				</div>
+				<nav className={styles.menu}>
+					<ul>
+						<li>
+							<a href="#aboutMe">About me</a>
+						</li>
+						<li>
+							<a href="#projects">Projects</a>
+						</li>
+						<li>
+							<a href="#skills">Skills</a>
+						</li>
+						<li>
+							<a className={styles.contact} href="#contact">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</nav>
 			</div>
-			<nav className={styles.menu}>
-				<ul>
-					<li>
-						<a href="#aboutMe">About me</a>
-					</li>
-					<li>
-						<a href="#projects">Projects</a>
-					</li>
-					<li>
-						<a href="#skills">Skills</a>
-					</li>
-					<li>
-						<a className={styles.contact} href="#contact">
-							Contact
-						</a>
-					</li>
-				</ul>
-			</nav>
 		</header>
 	);
 }
